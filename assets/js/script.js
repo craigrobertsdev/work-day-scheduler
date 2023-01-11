59a1ce506f06b709a10b84cf938f7176059b0224
Craig Roberts <craig.roberts11@outlook.com>
2023-01-12 07:04:15 +1030

    section colours based on time now working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,51 +2,81 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  // get all hour container divs
+  const hourSections = $('div.container-lg > div');
+  // Displays the date at the top of the page
+  displayDate();
+  setHourColours();
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
+  // add event listeners to each hour container
+  for (section of hourSections) {
+    $(section).find('button').click(event, handleSave);
+  }
+
+  // updates colours then sets timer to change as hour changes
+  function setHourColours() {
+    const nextHour = dayjs().startOf('h').add(1, 'hour');
+    const diff = nextHour.diff(dayjs());
+    updateColours();
+    setTimeout(() => {
+      updateColours();
+      diff = 3600000;
+      setTimeout(updateColours, diff);
+    }, diff);
+  }
+
+  function updateColours() {
+    // iterate over the hour containers and compare their hour to the current one. set class appropriately
+    for (section of hourSections) {
+      let sectionHour = parseInt($(section).attr('id').split('-')[1]);
+      let currentHour = dayjs().hour();
+
+      if (sectionHour === currentHour) {
+        $(section).addClass('present');
+      } else if (sectionHour < currentHour) {
+        $(section).addClass('past');
+      } else {
+        $(section).addClass('future');
+      }
+    }
+  }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  
-  // TODO: Add code to display the current date in the header of the page.
-  displayDate();
-  //flag to change diff to 24 hours if the day has changed when the call to setInterval is made
-
-  // this function updates the date only when the day changes. Done in this way to save on having to process the entire function every second
-  function displayDate() {
-    const startOfTomorrow = dayjs().startOf('d').add(24, 'hour');
-
-    // determine how long until tomorrow
-    let diff = startOfTomorrow.diff(dayjs());
-    let currentDate = dayjs().format('dddd, MMMM D');
-
-    // get ordinal suffix for date
-    let suffix = ordinalSuffixOf(dayjs().date());
-    // display current date
-    $('#currentDay').text(currentDate + '' + suffix);
-
-    // update date and set timeout to run every 24 hours from then on
-    setTimeout(function timer() {
-      suffix = ordinalSuffixOf(dayjs().date());
-      currentDate = dayjs().format('dddd, MMMM D');
-      $('#currentDay').text(currentDate + '' + suffix);
-      diff = 86400000;
-      setTimeout(timer, diff);
-    }, diff);
-  }
 });
+
+// displays the current date in the header section
+// this function updates the date only when the day changes. Done in this way to save on having to process the entire function every second
+function displayDate() {
+  const startOfTomorrow = dayjs().startOf('d').add(24, 'hour');
+
+  // determine how long until tomorrow
+  let diff = startOfTomorrow.diff(dayjs());
+
+  // display current date
+  displayDay();
+
+  // set timeout to run every 24 hours from then on
+  setTimeout(function () {
+    displayDay();
+    // number of milliseconds in a day
+    let diff = 86400000;
+    setTimeout(displayDay, diff);
+  }, diff);
+}
+
+function displayDay() {
+  const suffix = ordinalSuffixOf(dayjs().date());
+  const currentDate = dayjs().format('dddd, MMMM D');
+  $('#currentDay').text(currentDate + '' + suffix);
+}
 
 // determines which ordinal suffix to return based on the date provided
 function ordinalSuffixOf(i) {
@@ -62,4 +92,18 @@ function ordinalSuffixOf(i) {
     return 'rd';
   }
   return 'th';
+}
+
+function handleSave(event) {
+  event.preventDefault();
+  const textarea = $(this).siblings('textarea')[0];
+  const text = $(textarea).val();
+  const id = $(this).parent('div.time-block').attr('id');
+
+  save(id, text);
+}
+
+function save(id, text) {
+  console.log('calling save' + id + ' ' + text);
+  localStorage.setItem(id, text);
 }
